bb7bafa15875d7de7ad814e06d1de7eeeb7c3633
Rowlet <2474095154@qq.com>
2023-02-26 11:27:15 +0800

    add swagger

--- server/handler/handler.go
+++ server/handler/handler.go
@@ -1,1 +1,10 @@
 package handler
+
+import "github.com/gin-gonic/gin"
+
+func UserLogin(c *gin.Context) {
+
+}
+func UserRegister(c *gin.Context) {
+
+}
--- server/router/router.go
+++ server/router/router.go
@@ -1,1 +1,16 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"server/handler"
+)
+
+func InitRouters() *gin.Engine {
+	r := gin.Default()
+	rootPath := r.Group("/Student_Management_System")
+	//用户注册
+	rootPath.POST("user/register", handler.UserRegister)
+	//用户登录
+	rootPath.POST("user/login", handler.UserLogin)
+	return r
+}
