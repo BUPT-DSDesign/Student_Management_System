a45c2934045308986a02ef45c36474f9e84ef8d2
jhc <1547676099@qq.com>
2023-02-26 10:12:40 +0800

    暂时去掉api，add some utils类

---
+++ server/go.mod
@@ -0,0 +1,7 @@
+module server
+
+go 1.19
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+
+require github.com/bwmarrin/snowflake v0.3.0
---
+++ server/utils/snowflake.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/bwmarrin/snowflake"
+)
+
+// 雪花算法生成用户Id
+func GenerateId() (int64, error) {
+	// 创建一个节点
+	node, err := snowflake.NewNode(time.Now().Unix()%1024 + 1)
+	if err != nil {
+		return int64(0), err
+	}
+
+	return node.Generate().Int64(), nil
+}
