ebb2c1be733b1eac5b886d1ce531566e716d28c4
jhc <1547676099@qq.com>
2023-02-26 11:52:48 +0800

    路由分组，handler目录

--- server/router/router.go
+++ server/router/router.go
@@ -1,16 +1,23 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
 	"server/handler"
+
+	"github.com/gin-gonic/gin"
 )
 
 func InitRouters() *gin.Engine {
 	r := gin.Default()
 	rootPath := r.Group("/Student_Management_System")
-	//用户注册
-	rootPath.POST("user/register", handler.UserRegister)
-	//用户登录
-	rootPath.POST("user/login", handler.UserLogin)
+
+	// 用户路由
+	userGroup := rootPath.Group("/user")
+	{
+		//用户注册
+		userGroup.POST("/register", handler.UserRegister) // 后续使用中间件
+		//用户登录
+		userGroup.POST("/login", handler.UserLogin)
+	}
+
 	return r
 }
