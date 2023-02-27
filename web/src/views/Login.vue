d87a23a565868d3d2220b8f24fee2cf210354715
zhiruozzy <2490932295@qq.com>
2023-02-27 23:13:18 +0800

    update

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,83 +1,98 @@
+<!-- 登录页面 -->
 <template>
     <div class="login-container">
         <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-            <el-form-item label="用户名" prop="name">
-                <el-input v-model="ruleForm.name"></el-input>
+            <el-form-item label="用户名" prop="usename">
+                <el-input v-model="ruleForm.usename"></el-input>
             </el-form-item>
             <el-form-item label="密码" prop="password">
-                <el-input type="password"  v-model="ruleForm.password"></el-input>
+                <el-input type="password" v-model="ruleForm.password"></el-input>
             </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-            </el-form-item>
-
+            <el-link type="primary" @click="jumpReg">注册账号</el-link>
+            <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
         </el-form>
     </div>
 </template>
 
 <script>
 import { LoginIn } from '@/pinia/modules/user'
-
-
-
+import axios from 'axios';
 export default {
     data() {
         return {
             ruleForm: {
-                name: 'admin',
-                password: '123456',
+                usename: '',
+                password: '',
             },
             rules: {
-                name: [
+                usename: [
                     { required: true, message: '请输入用户名', trigger: 'blur' },
+                    { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
                 ],
                 password: [
                     { required: true, message: '请输入密码', trigger: 'blur' },
+                    { min: 6, max:8, message: '长度在 6 到 8 个字符', trigger: 'blur' }
                 ],
 
             }
         };
     },
     methods: {
-        LoginIn: async function() {
-            console.dir(this)
-            return await LoginIn(this.ruleForm) 
-        },
+        // LoginIn: async function () {
+        //     return await LoginIn(this.ruleForm) 
+        // },
         submitForm(formName) {
-            this.$refs[formName].validate(async(valid) => {
+            let that = this;
+            this.$refs[formName].validate((valid) => {
                 if (valid) {
-                    const flag = await this.LoginIn()
-                    alert('submit!');
-
+                    //post的第一个参数：传入后端的地址，第二个参数：登录数据
+                    axios.post("http://localhost:8080/Student_Management_System/user/login", {
+                        usename: this.ruleForm.usename,
+                        password: this.ruleForm.password
+                    }).then
+                        (function (response) {
+                            let data = response.data;
+                            if (data.status_code == 0 && data.status_msg == '登录成功') {
+                                that.$router.push('/Main/Homepage');
+                            }
+                        }, function (err) {
+                            console.log(err);
+                        })
                 } else {
                     console.log('error submit!!');
                     return false;
                 }
             });
         },
+        jumpReg() {
+            this.$router.push('/Register');
+        }
     }
 }
 </script>
 <style lang="scss" scoped>
-.login-container{
+.login-container {
     background-color: #ccc;
     height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
 }
-.el-form{
-    width: 300px;
+
+.el-form {
+    width: 400px;
     background-color: #fff;
     padding: 30px;
     border-radius: 10px;
 }
-.el-form-item{
-    margin-top:20px;
+
+.el-form-item {
+    margin-top: 20px;
 }
-.el-button{
-    width:100%;
-    margin-top:20px;
+
+.el-button {
+    width: 100%;
+    margin-top: 20px;
 }
 </style>
 
--- web/src/views/Register.vue
+++ web/src/views/Register.vue
@@ -1,66 +1,117 @@
 <template>
-    <div>
-        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-          <el-form-item label="用户名" prop="name">
-            <el-input v-model="ruleForm.name"></el-input>
-          </el-form-item>
-          <el-form-item label="密码" prop="password">
-                <el-input v-model="ruleForm.password"></el-input>
-              </el-form-item>
-        </el-form>
+    <div class="container">
+        <div class="register">
+            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+                <el-form-item label="用户名" prop="usename">
+                    <el-input v-model="ruleForm.usename"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password">
+                    <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="确认密码" prop="checkPass">
+                    <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" style="margin-right: 60px;" @click="submitForm('ruleForm')">提交</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 
 <script>
-     export default {
+import axios from 'axios';
+export default {
     data() {
+        var checkName = (rule, value, callback) => {
+            if (!value) {
+                return callback(new Error('用户名不能为空'));
+            }
+            else {
+                callback();
+            }
+        };
+        var validatePass = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请输入密码'));
+            } else {
+                if (this.ruleForm.checkPass !== '') {
+                    this.$refs.ruleForm.validateField('checkPass');
+                }
+                callback();
+            }
+        };
+        var validatePass2 = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请再次输入密码'));
+            } else if (value !== this.ruleForm.password) {
+                callback(new Error('两次输入密码不一致!'));
+            } else {
+                callback();
+            }
+        };
         return {
             ruleForm: {
-                name: '',
                 password: '',
+                checkPass: '',
+                usename: ''
             },
             rules: {
-                name: [
-                    { required: true, message: '请输入用户名', trigger: 'blur' },
-                    { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+                usename: [
+                    { validator: checkName, trigger: 'blur' }
                 ],
-                password: [
-                    { required: true, message: '请输入密码', trigger: 'blur' },
-                    { min: 6, max: 10, message: '长度在 6 到 10 个字符', trigger: 'blur' }
+                pass: [
+                    { validator: validatePass, trigger: 'blur' }
                 ],
-                date1: [
-                    { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
-                ],
-                date2: [
-                    { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
-                ],
-                type: [
-                    { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
-                ],
-                resource: [
-                    { required: true, message: '请选择活动资源', trigger: 'change' }
-                ],
-                desc: [
-                    { required: true, message: '请填写活动形式', trigger: 'blur' }
+                checkPass: [
+                    { validator: validatePass2, trigger: 'blur' }
                 ]
             }
         };
     },
     methods: {
         submitForm(formName) {
+            let that = this;
             this.$refs[formName].validate((valid) => {
                 if (valid) {
-                    alert('submit!');
+                    //post的第一个参数：传入后端的地址，第二个参数：登录数据
+                    axios.post("http://localhost:8080/Student_Management_System/user/register", {
+                        usename: this.ruleForm.usename,
+                        password: this.ruleForm.password
+                    }).then
+                        (function (response) {
+                            let data = response.data;
+                            console.log(response);
+                            if (data.status_code == 0 && data.status_msg == '注册成功') {
+                                that.$router.push('/Main/Homepage');
+                            }
+                        }, function (err) {
+                            console.log(err);
+                        })
                 } else {
                     console.log('error submit!!');
                     return false;
                 }
             });
-        },
-        resetForm(formName) {
-            this.$refs[formName].resetFields();
         }
     }
 }
 </script>
 
+<style scoped>
+.container{
+    background-color:#ccc;
+    height: 100vh;
+     display: flex;
+}
+.register {
+    background-color:#fff;
+    width:400px;
+    height: 250px;
+    margin-top: 100px;
+    margin-left:400px;
+    padding:50px;
+    border-radius: 10px;
+}
+
+</style>
