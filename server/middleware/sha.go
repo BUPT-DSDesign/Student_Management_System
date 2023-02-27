f1f2540b07a4112321f4bad083f1a0a300efe67f
Rowlet <2474095154@qq.com>
2023-02-27 20:38:33 +0800

    修改了SHA相关结构，新增掺盐函数

--- server/middleware/sha.go
+++ server/middleware/sha.go
@@ -1,30 +1,17 @@
 package middleware
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"server/model/entity/common"
-
-	"github.com/gin-gonic/gin"
+	"server/utils"
 )
-
-// 使用sha1对密码加密
-func sha(s string) (string, error) {
-	hash := sha1.New()
-	if _, err := hash.Write([]byte(s)); err != nil {
-		return "", err
-	}
-
-	return hex.EncodeToString(hash.Sum(nil)), nil
-}
 
 func ShaMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		rawPassword := c.PostForm("password")
-
 		// 密码加密
-		password, err := sha(rawPassword)
+		password, err := utils.Sha1(rawPassword)
 		if err != nil {
 			c.JSON(http.StatusOK, common.StatusResponse{
 				StatusCode: 403,
---
+++ server/utils/sha.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"math/rand"
+	"time"
+)
+
+// SHA1加密
+func Sha1(s string) (string, error) {
+	hash := sha1.New()
+	if _, err := hash.Write([]byte(s)); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(hash.Sum(nil)), nil
+}
+
+// SHA256加密
+func Sha256(s string) (string, error) {
+	hash := sha256.New()
+	if _, err := hash.Write([]byte(s)); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(hash.Sum(nil)), nil
+}
+
+// 盐值生成
+func GenerateSalt(size int) (string, error) {
+	if size > 10 || size <= 0 {
+		return "", errors.New("非法长度,长度应为1-10之间的整数")
+	}
+	randomNum := []byte{33, 126}
+	var result string
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		result += string(randomNum)
+	}
+	return result, nil
+}
