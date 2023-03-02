6c72d3c37d39fce28e9c35a47f2c9bb6cd3dab2d
zhiruozzy <2490932295@qq.com>
2023-03-02 12:16:16 +0800

    前端地图路径显示

--- web/src/main.js
+++ web/src/main.js
@@ -4,10 +4,20 @@ import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from './router';
 import axios from 'axios'
+import AMap from 'vue-amap'
+
 
 Vue.prototype.$http = axios //将axios挂载到原型上，以供全局使用
 
 Vue.use(ElementUI);
+
+Vue.use(AMap)
+AMap.initAMapApiLoader({
+  key: '645785d3ed8cd58933f1e27cd08672cb',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  // 默认高德 sdk 版本为 1.4.4
+  v: '1.4.4'
+})
 
 Vue.config.productionTip = false
 
--- web/src/views/CourseNav.vue
+++ web/src/views/CourseNav.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <div id="container" style="width:100%;height:90vh" />
+        <!-- 
         <div class="bgd"></div>
         <div class="search">
             <el-form ref="form" :model="form" label-width="80px">
@@ -30,28 +32,82 @@
                     <el-button>取消</el-button>
                 </el-form-item>
             </el-form>
-        </div>
+        </div> -->
     </div>
 </template>
 <script>
+// 绘制线路需要的坐标
+var lineArr = [[116.35530714718364, 39.96393072423919], [116.35542348293764, 39.964436412717816], [116.35600217544192, 39.9646045260412]];
+// var lineArr = [[116.478935, 39.997761], [108.983569, 34.285675], [103.85094, 35.987496], [106.205794, 38.458831], [111.761777, 40.875595]]
 export default {
     data() {
         return {
-            form: {
-                name: '',
-                region: '',
-                date1: '',
-                date2: '',
-                delivery: false,
-                type: [],
-                resource: '',
-                desc: ''
-            }
+            firstArr: [116.35530714718364, 39.96393072423919],// 中心点/初始坐标
+
+            // form: {
+            //     name: '',
+            //     region: '',
+            //     date1: '',
+            //     date2: '',
+            //     delivery: false,
+            //     type: [],
+            //     resource: '',
+            //     desc: ''
+            // }
         }
+    },
+    created() { },
+    mounted() {
+        setTimeout(() => {
+            this.initMap() // 异步加载（否则报错initMap is not defined）
+            // this.initroad()
+        }, 1000)
     },
     methods: {
         onSubmit() {
             console.log('submit!');
+        },
+         // 初始化地图
+        initMap() {
+            var that = this
+            this.map = new AMap.Map('container', {
+                resizeEnable: true, // 窗口大小调整
+                center: this.firstArr, 
+            })
+            // 添加maker
+            this.marker = new AMap.Marker({
+                map: this.map,
+                position: this.firstArr,
+                icon: 'https://webapi.amap.com/images/car.png',
+                offset: new AMap.Pixel(-26, -13), // 调整图片偏移
+                autoRotation: true, // 自动旋转
+                angle: -90 // 图片旋转角度
+            })
+            that.initroad()
+        },
+        // 初始化轨迹
+        initroad() {
+            // 绘制还未经过的路线
+            this.polyline = new AMap.Polyline({
+                map: this.map,
+                path: lineArr,
+                showDir: true,
+                strokeColor: '#77DDFF', // 线颜色--浅蓝色
+                // strokeOpacity: 1,     //线透明度
+                strokeWeight: 6, // 线宽
+                // strokeStyle: "solid"  //线样式
+                lineJoin: 'round' // 折线拐点的绘制样式
+            })
+            // 绘制路过了的轨迹
+            var passedPolyline = new AMap.Polyline({
+                map: this.map,
+                strokeColor: '#00BBFF', // 线颜色-深蓝色
+                path: [[116.35530714718364, 39.96393072423919], [116.35542348293764, 39.964436412717816], [116.35600217544192, 39.9646045260412]],
+                // strokeOpacity: 1,     //线透明度
+                strokeWeight: 6 // 线宽
+                // strokeStyle: "solid"  //线样式
+            })
+            this.map.setFitView() // 合适的视口
         }
     }
 }
@@ -68,7 +124,7 @@ export default {
 }
 
 .search {
-    margin-top:50px;
+    margin-top: 50px;
     float: right;
 }
 </style>
