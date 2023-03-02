b3de538de226fd6cd69f3833874040d7e0d191be
Rowlet <2474095154@qq.com>
2023-03-02 12:36:35 +0800

    开了dijkstra和heapsort的坑

---
+++ server/algorithm/dijkstra.go
@@ -0,0 +1,49 @@
+package algorithm
+
+type Edge struct {
+	next   int
+	to     int
+	length float64
+}
+type Path struct {
+	fromId   int
+	descId   int
+	distance float64
+}
+
+// Dijkstra的dis数组
+type NodeDis struct {
+	nodeId int
+	dis    float64
+}
+
+// 前向星存图加边
+func addEdge(path []Path, head []int) ([]int, []Edge) {
+	edges := make([]Edge, len(path)*2+5)
+	edgeCnt := 0
+	for _, i := range path {
+		//无向图加边
+		edges[edgeCnt].to = i.descId
+		edges[edgeCnt].length = i.distance
+		edges[edgeCnt].next = head[i.fromId]
+		head[i.fromId] = edgeCnt
+		edgeCnt++
+		edges[edgeCnt].to = i.fromId
+		edges[edgeCnt].length = i.distance
+		edges[edgeCnt].next = head[i.descId]
+		head[i.descId] = edgeCnt
+		edgeCnt++
+	}
+	return head, edges
+}
+func Dijkstra(start int, desc int, path []Path, node_cnt int) (pathList []int, err error) {
+	//使用前向星存图
+	head := make([]int, node_cnt+5)
+	//初始化为-1
+	for i, _ := range head {
+		head[i] = -1
+	}
+	//获取前向星存图
+	head, _ = addEdge(path, head)
+	return
+}
