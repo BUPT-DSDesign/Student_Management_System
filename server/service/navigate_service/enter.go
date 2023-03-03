d76e0694db609f8e976011255bbab7d8d70085a6
Rowlet <2474095154@qq.com>
2023-03-04 00:29:27 +0800

    修改循环定义,合并

--- server/service/navigate_service/enter.go
+++ server/service/navigate_service/enter.go
@@ -1,7 +1,7 @@
 package navigate_service
 
 import (
-	"server/handler/navigate_handler"
+	"server/model/entity/common"
 	"server/model/entity/system"
 )
 
@@ -9,7 +9,7 @@ type NavigateServer interface {
 	// DoNavigation 开始导航,返回导航路径
 	DoNavigation(navigateRequest common.NavigateRequest) ([]system.Coordinate, error)
 	// DoGetFacilityList 返回待查询建筑物列表
-	DoGetFacilityList(request system.FacilityRequest) ([]navigate_handler.FacilitiyInfo, error)
+	DoGetFacilityList(request common.FacilityRequest) (*[]*system.FacilityInfo, error)
 }
 type server struct {
 	tempNodeList []Node
