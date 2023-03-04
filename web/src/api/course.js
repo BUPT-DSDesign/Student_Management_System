05be252ad62ca03bccbc2f06a00081ab0816a8b0
jhc <1547676099@qq.com>
2023-03-04 17:43:15 +0800

    课程表接口, 有bug

---
+++ web/src/api/course.js
@@ -0,0 +1,9 @@
+import { service } from '@/utils/request'
+
+// 课程表接口
+export const CourseTable = () => {
+    return service({
+        url: 'course/table',
+        method: 'get'
+    })
+}
--- web/src/views/inClass.vue
+++ web/src/views/inClass.vue
@@ -74,6 +74,7 @@
     </div>
 </template>
 <script>
+import { bus } from '@/transfer/bus'
 export default {
     data() {
         return {
@@ -101,6 +102,11 @@ export default {
             filclasslist: [], //模糊匹配后的课程列表
 
         };
+    },
+    created() {
+        bus.$on("courseList", (data) => {   //这里最好用箭头函数，不然this指向有问题
+            this.classData = data
+        })
     },
     watch: {
         keyWord(newvalue) {
