8fbf0cd94bbcecf9bd2ee516add357e62af316a0
zhiruozzy <2490932295@qq.com>
2023-03-04 17:17:18 +0800

    活动界面

---
+++ web/src/components/eventDialog.vue
@@ -0,0 +1,30 @@
+<template>
+    <el-dialog title="课程详情" :visible.sync="dialogDetailVisible" width="30%" :before-close="handleClose">
+        <el-card class="box-card">
+            <div>活动名称：{{ selected.event }}</div>
+            <div>活动周次：{{ selected.week }}</div>
+            <div>活动地点：{{ selected.address }}</div>
+            <div>活动类型：{{ selected.tag }}</div>
+            <div>活动开始时间：{{ selected.time }}</div>
+        </el-card>
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogDetailVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogDetailVisible = false">确 定</el-button>
+        </span>
+    </el-dialog>
+</template>
+<script>
+   export default {
+    data() {
+           return {
+        }
+    },
+    props: ['selected','dialogDetailVisible'],//接收父组件传过来的信息
+    method: {
+         // 关闭弹窗
+        handleClose(done) {
+            done();
+        },
+    }
+}
+</script>
--- web/src/views/outClass.vue
+++ web/src/views/outClass.vue
@@ -1,133 +1,187 @@
 <template>
-    <el-tabs type="border-card">
-        <el-tab-pane>
-            <span slot="label"><i class="el-icon-date"></i> 个人活动</span>
-            <div class="container">
-                <div class="schedule-container">
-                    <h2>日程安排表</h2>
-                    <div>
-                        <el-table :data="eventlist.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
-                            style="width: 100%"  max-height="350">
-                            <el-table-column label="日期" prop="date"  fixed  sortable :sort-method="(a, b) => a.date.localeCompare(b.date)"  >
-                            </el-table-column>
-                            <el-table-column label="时间" prop="time" fixed sortable :sort-method="(a, b) => a.time.localeCompare(b.time)">
-                            </el-table-column>
-                            <el-table-column label="事件" prop="event" fixed >
-                            </el-table-column>
-                            <el-table-column align="right">
-                                <template slot="header" slot-scope="scope">
-                                    <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
-                                </template>
-                                <template slot-scope="scope">
-                                    <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-                                    <el-button size="mini" type="danger"
-                                        @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-                                </template>
-                            </el-table-column>
-                        </el-table>
-                    </div>
-                </div>
-                <!-- 按键部分 -->
-                <div class="button-container">
-                    <!-- 添加活动 -->
-                    <el-button class="OperationButton" type="primary" icon="el-icon-circle-plus-outline"
-                        @click="dialogFormVisible = true">添加活动</el-button>
-                    <el-dialog title="添加活动" :visible.sync="dialogFormVisible">
-                        <el-form :model="form">
-                            <el-form-item label="活动名称" :label-width="formLabelWidth">
-                                <el-input v-model="form.name" autocomplete="off"></el-input>
-                            </el-form-item>
-                            <el-form-item label="活动地点" :label-width="formLabelWidth">
-                                <el-input v-model="form.address" autocomplete="off"></el-input>
-                            </el-form-item>
-                            <el-form-item label="活动起止时间" :label-width="formLabelWidth">
-                                <el-time-picker is-range v-model="value1" range-separator="至" start-placeholder="开始时间"
-                                    end-placeholder="结束时间" placeholder="选择时间范围">
-                                </el-time-picker>
-                            </el-form-item>
-                            <el-form-item label="活动类型" :label-width="formLabelWidth">
-                                <el-radio v-model="radio" label="1">个人活动</el-radio>
-                                <el-radio v-model="radio" label="2">集体活动</el-radio>
-                            </el-form-item>
-                        </el-form>
-                        <div slot="footer" class="dialog-footer">
-                            <el-button @click="dialogFormVisible = false">取 消</el-button>
-                            <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-                        </div>
-                    </el-dialog>
-                    <el-button class="OperationButton" type="primary" icon="el-icon-search">活动查询</el-button>
-                    <el-button class="OperationButton" type="primary" icon="el-icon-alarm-clock">闹钟提醒</el-button>
-                </div>
+    <div class="container">
+        <div class="schedule-container">
+            <h2>日程安排表</h2>
+            <div>
+                <el-table
+                    :data="eventlist.filter(data => !search || data.event.includes(search) || data.week.includes(search) || data.time.includes(search))"
+                    style="width: 100%" max-height="350">
+                    <el-table-column label="日期" prop="week" fixed sortable
+                        :sort-method="(a, b) => a.week.localeCompare(b.week)">
+                    </el-table-column>
+                    <el-table-column label="时间" prop="time" fixed sortable
+                        :sort-method="(a, b) => a.time.localeCompare(b.time)">
+                    </el-table-column>
+                    <el-table-column label="事件" prop="event" fixed>
+                    </el-table-column>
+                    <el-table-column prop="tag" label="标签" width="100"
+                        :filters="[{ text: '集体活动', value: '集体活动' }, { text: '个人活动', value: '个人活动' }]"
+                        :filter-method="filterTag" filter-placement="bottom-end">
+                        <template slot-scope="scope">
+                            <el-tag :type="scope.row.tag === '集体活动' ? 'primary' : 'success'" disable-transitions>{{
+                                scope.row.tag }}</el-tag>
+                        </template>
+                    </el-table-column>
+                    <el-table-column align="right">
+                        <template slot="header" slot-scope="scope">
+                            <el-input v-model="search" size="mini" placeholder="输入关键词(时间/事件)搜索" />
+                        </template>
+                        <template slot-scope="scope">
+                            <el-button type="primary" size="mini" @click="handleClick(scope.row)">查看详情</el-button>
+                            <!-- <el-button type="info" size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button> -->
+                            <el-button type="danger" size="mini"
+                                @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
             </div>
-        </el-tab-pane>
-        <el-tab-pane>
-            <span slot="label"><i class="el-icon-s-flag"></i> 集体活动</span>
-            集体活动
-        </el-tab-pane>
-    </el-tabs>
+        </div>
+        <eventDialog :selected="selected" :dialogDetailVisible="dialogDetailVisible"></eventDialog>
+        <!-- 按键部分 -->
+        <div class="button-container">
+            <!-- 添加活动按钮和弹窗 -->
+            <el-button class="OperationButton" type="primary" icon="el-icon-circle-plus-outline"
+                @click="dialogAddVisible = true">添加活动</el-button>
+            <el-dialog title="添加活动" :visible.sync="dialogAddVisible">
+                <el-form :model="form">
+                    <el-form-item label="活动名称" :label-width="formLabelWidth">
+                        <el-input v-model="form.event" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="活动地点" :label-width="formLabelWidth">
+                        <el-input v-model="form.address" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="开始周次" :label-width="formLabelWidth">
+                        <el-input v-model="form.startweek" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="结束周次" :label-width="formLabelWidth">
+                        <el-input v-model="form.endweek" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="活动时间" :label-width="formLabelWidth">
+                        <el-input v-model="form.time" autocomplete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="活动类型" :label-width="formLabelWidth">
+                        <el-radio v-model="radio" label="1">个人活动</el-radio>
+                        <el-radio v-model="radio" label="2">集体活动</el-radio>
+                    </el-form-item>
+                </el-form>
+                <div slot="footer" class="dialog-footer">
+                    <el-button @click="dialogAddVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="dialogAddVisible = false">确 定</el-button>
+                </div>
+            </el-dialog>
+            <!-- 闹钟提醒 -->
+            <el-button class="OperationButton" type="primary" icon="el-icon-alarm-clock">闹钟提醒</el-button>
+        </div>
+    </div>
 </template>
 
 <script>
+import eventDialog from '../components/eventDialog.vue'  //引入弹窗组件
 export default {
     data() {
         return {
-            dialogFormVisible: false,
+            selected: {},//选择查看详情的活动对象
+            dialogDetailVisible: false,
+            dialogAddVisible: false, //添加活动的弹窗是否可见
+            //增加活动时表单中填的数据
             form: {
-                name: '',
+                week: '',
+                time: '',
+                event: '',
+                tag: '',
                 address: '',
+                startweek: '',
+                endweek: '',
             },
-            search: '',
+            radio: '',
+            search: '', //用于搜索过滤的对象
             formLabelWidth: '120px',
             value1: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)],
             eventlist: [{
-                date: '周三',
+                week: '周3',
                 time: '15:00',
-                event: '班会'
+                event: '小组作业',
+                tag: "集体活动",
+                address: "教室",
             }, {
-                date: '周四',
+                week: '周4',
                 time: '14:00',
-                event: '取快递'
+                event: '取快递',
+                address: "教室",
+                tag: "个人活动",
             }, {
-                date: '周五',
+                week: '周5',
                 time: '14:00',
-                event: '计算机网络考试'
+                event: '计算机网络考试',
+                address: "教室",
+                tag: "集体活动",
             }, {
-                date: '周五',
+                week: '周5',
                 time: '17:00',
-                event: '出门游玩'
+                event: '出门游玩',
+                address: "教室",
+                tag: "集体活动",
+
             }, {
-                date: '周一',
+                week: '周1',
                 time: '14:00',
-                event: '游玩'
+                event: '锻炼',
+                address: "教室",
+                tag: "个人活动",
+
             }, {
-                date: '周二',
+                week: '周2',
                 time: '12:00',
-                event: '去超市'
+                event: '去超市',
+                address: "教室",
+                tag: "个人活动",
+
             }, {
-                date: '周四',
-                time: '7:00',
-                event: '买菜'
+                week: '周4',
+                time: '07:00',
+                event: '创新创业',
+                address: "教室",
+                tag: "个人活动",
             }, {
-                date: '周三',
+                week: '周3',
                 time: '11:00',
-                event: '开班会'
+                event: '开班会',
+                address: "教室",
+                tag: "集体活动",
+
             },]
         }
     },
     methods: {
-        formatter(row, column) {
-            return row.name;
-        },
-        handleEdit(index, row) {
-            console.log(index, row);
-        },
+        //删除活动按钮,row即为活动对象
         handleDelete(index, row) {
-            console.log(index, row);
+            console.log(row);
         },
+        //对标签进行筛选
+        filterTag(value, row) {
+            return row.tag === value;
+        },
+        // 查看详情按钮
+        handleClick(row) {
+            this.selected = row;
+            this.dialogDetailVisible = true;
+        },
+    },
+    components: {
+        eventDialog,
     }
 }
 </script>
 <style>
+.schedule-container {
+    margin-top: 40px;
+}
+
+.el-input__inner {
+    height: 39px;
+    line-height: 30px;
+}
+
 h2 {
     margin: 15px;
     margin-left: 200px;
@@ -135,10 +189,16 @@ h2 {
 }
 
 .button-container {
-    margin-top: 10px;
+    margin-top: 20px;
+    margin-left: 130px;
 }
 
 .OperationButton {
     margin: 20px
+}
+
+.weekinput {
+    width: 30px;
+    margin: 0 5px;
 }
 </style>
