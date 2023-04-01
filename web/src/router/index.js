4d8050faa252a9e1faef3791d7cb4ce523123699
zhiruozzy <2490932295@qq.com>
2023-04-01 22:09:35 +0800

    增加管理员

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -8,7 +8,9 @@ import CourseNav from '../views/CourseNav.vue'
 import Homepage from '../views/Homepage.vue'
 import inClass from '../views/inClass.vue'
 import outClass from '../views/outClass.vue'
-import Main from '../views/Main.vue'
+import userMain from '../views/userMain.vue'
+import adminMain from '../views/adminMain.vue'
+import classManage from '../views/classManage.vue'
 import Register from '../views/Register.vue'
 import Login from '../views/Login.vue'
 import { Message } from 'element-ui'
@@ -17,28 +19,35 @@ Vue.use(VueRouter)
 
 //2.将路由与组件进行映射,path是网页中的路径，而component是组件的名称
 const routes = [
-    //主路由
     {
-        path: '/Main',
-        component: Main,
+        path: '/userMain',
+        component: userMain,
+        meta: { roles: ['user'] },
         //嵌套路由
         children: [
-            // { path: 'ScheduleMge',component: ScheduleMge,},
-            { path: 'inClass', component: inClass },
-            { path: 'outClass', component: outClass },
-            { path: 'CourseNav', component: CourseNav },
-            { path: 'Homepage', component: Homepage },
+            { path: 'inClass', component: inClass, meta: { roles: ['user'] } },
+            { path: 'outClass', component: outClass, meta: { roles: ['user'] } },
+            { path: 'CourseNav', component: CourseNav, meta: { roles: ['user'] } },
+            { path: 'Homepage', component: Homepage, meta: { roles: ['user'] } },
         ]
     },
-    { path: '/', component: Login, },
-    { path: '/Register', component: Register, },
-    
+    {
+        path: '/adminMain',
+        component: adminMain,
+        meta: { roles: ['admin'] },
+        //嵌套路由
+        children: [
+            { path: 'classManage', component: classManage, meta: { roles: ['admin'] } },
+        ]
+    },
+    { path: '/', component: Login, meta: { roles: ['user','admin'] } },
+    { path: '/Register', component: Register, meta: { roles: ['user', 'admin'] } },
 ]
 
 //3.创建router实例
 const router = new VueRouter({
     routes,
-    mode:'history'
+    mode: 'history'
 })
 
 // 4.设置用户进入页面的权限, 当用户处于登录状态时, 可以直接进入其他页面, 否则会跳转到登录页面
---
+++ web/src/views/adminMain.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <el-container>
+            <el-aside width="200px">
+                <!-- 侧边栏 -->
+                <adminAside :routes="getRoutes"></adminAside>
+            </el-aside>
+            <el-container>
+                <el-header>
+                    <!-- 上方 -->
+                    <Header></Header>
+                </el-header>
+                <el-main>
+                    <!-- 子路由的路由出口 -->
+                    <router-view></router-view>
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+<script>
+import Aside from '../components/userAside.vue'
+import Header from '../components/Header.vue'
+
+export default {
+    data() {
+        return {
+        }
+    },
+    computed: {
+        getRoutes() {
+            return global.antRouter
+        }
+    },
+    data() {
+        return {
+            userRole: 'Topest',
+        }
+    },
+    components: {
+        Aside,
+        Header,
+    }
+}
+</script>
+<style>
+.el-header {
+    padding: 0;
+}
+
+.el-main {
+    padding: 0;
+}
+</style>
+
