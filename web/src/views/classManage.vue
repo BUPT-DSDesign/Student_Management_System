ce9433b7af227c49b25453c3056e3fd6fb4cdefe
zhiruozzy <2490932295@qq.com>
2023-04-06 13:26:17 +0800

    管理员

--- web/src/views/classManage.vue
+++ web/src/views/classManage.vue
@@ -1,6 +1,6 @@
 <template>
-   <div style="margin:20px">
-     <h1>课程管理</h1>
+    <div style="margin:20px">
+        <h1>课程管理</h1>
         <el-table :data="tableData" height="250" border style="width: 400px">
             <el-table-column prop="classname" label="课程名称" width="180">
             </el-table-column>
@@ -9,45 +9,56 @@
         </el-table>
         <el-button type="primary" plain>修改课程</el-button>
         <el-button type="primary" plain>添加课程</el-button>
-   </div>
+    </div>
 </template>
 
 <script>
 import { CourseStore } from '@/store/course';
+// import { start } from 'repl';
 // import { clearScreenDown } from 'readline';
-const timemap = {
-    1: "8:00-9:00",
-    2: "9:00-10:00",
-    3: "10:00-11:00",
-    4: "11:00-12:00",
-    5: "12:00-13:00",
-    6: "13:00-14:00",
-    7: "14:00-15:00",
-    8: "15:00-16:00",
-    9: "16:00-17:00",
-    10: "17:00-18:00",
+const startTimeMap = {
+    1: "8:00",
+    2: "9:00",
+    3: "10:00",
+    4: "11:00",
+    5: "13:00",
+    6: "14:00",
+    7: "15:00",
+    8: "16:00",
+    9: "17:00",
+};
+const endTimeMap = {
+    1: "9:00",
+    2: "10:00",
+    3: "11:00",
+    4: "12:00",
+    5: "14:00",
+    6: "15:00",
+    7: "16:00",
+    8: "17:00",
+    9: "18:00",
 }
 export default {
-    beforeMount() {
-         // 当点击侧边栏的课内信息时, 会向后端发送请求, 后端返回课程表
+    mounted() {
+        // 当点击侧边栏的课内信息时, 会向后端发送请求, 后端返回课程表
         const getTable = async () => {
             const fg = await CourseStore.GetCourseTable();
         }
         getTable();
         this.classData = CourseStore.courseList
         //添加一个属性以便格式化课程时间
-        for (let i = 0; i < this.classData.length; i++){
-            for (let j = 0; j < this.classData[i].section_list.length; j++){
-                let classtime = timemap[this.classData[i].section_list[j]%10];
-                this.classData[i]['classtime']= classtime;
-            }
+        for (let i = 0; i < this.classData.length; i++) {
+            let startTime = startTimeMap[this.classData[i].section_list[0] % 10];
+            let endTime = endTimeMap[this.classData[i].section_list.slice(-1) % 10];
+            this.classData[i]['classTime'] = startTime+'-'+endTime;
+
         }
         console.log(this.classData);
-        for (let i = 0; i < this.classData.length; i++){
+        for (let i = 0; i < this.classData.length; i++) {
             this.tableData.push({
                 classname: this.classData[i].course_name,
-                classtime:this.classData[i].classtime,
-                
+                classtime: this.classData[i].classTime,
+
             })
         }
     },
