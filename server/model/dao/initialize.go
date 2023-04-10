9ce8b6f3c74d1073bce15ab38d12c9b668a663a8
Rowlet <2474095154@qq.com>
2023-04-10 22:28:49 +0800

    加了go routing，修复了运行不了的BUG

--- server/model/dao/initialize.go
+++ server/model/dao/initialize.go
@@ -13,26 +13,28 @@ var stdinWriter *bufio.Writer
 var stdoutReader *bufio.Reader
 
 func init() {
-	cmd := exec.Command(filename)
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		panic(err)
-	}
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		panic(err)
-	}
-	if err := cmd.Start(); err != nil {
-		panic(err)
-	}
-	defer func(cmd *exec.Cmd) {
-		err := cmd.Wait()
+	go func() {
+		cmd := exec.Command(filename)
+		stdin, err := cmd.StdinPipe()
 		if err != nil {
 			panic(err)
 		}
-	}(cmd)
-	stdinWriter = bufio.NewWriter(stdin)
-	stdoutReader = bufio.NewReader(stdout)
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			panic(err)
+		}
+		if err := cmd.Start(); err != nil {
+			panic(err)
+		}
+		defer func(cmd *exec.Cmd) {
+			err := cmd.Wait()
+			if err != nil {
+				panic(err)
+			}
+		}(cmd)
+		stdinWriter = bufio.NewWriter(stdin)
+		stdoutReader = bufio.NewReader(stdout)
+	}()
 }
 
 func WriteLine(data string) error {
