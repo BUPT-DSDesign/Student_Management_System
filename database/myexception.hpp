5f70ddcfc1d8e75508b6155c9496d4f64a80eee3
Rowlet <2474095154@qq.com>
2023-05-14 00:28:47 +0800

    新增多个异常类型

--- database/myexception.hpp
+++ database/myexception.hpp
@@ -10,7 +10,7 @@ class SQLTypeError: public std::exception
 public:
     SQLTypeError(const std::string& msg):msg_(msg){}
     const char* what() const noexcept override{
-        return msg_.c_str();
+        return ("SQLTypeError:"+msg_).c_str();
     }
 private:
     std::string msg_;
@@ -22,7 +22,63 @@ class SQLSyntaxError: public std::exception
 public:
     SQLSyntaxError(const std::string& msg):msg_(msg){}
     const char* what() const noexcept override{
-        return msg_.c_str();
+        return ("SQLSyntaxError:"+msg_).c_str();
+    }
+private:
+    std::string msg_;
+};
+
+//在table中使用的报错
+class TableOpenError: public std::exception
+{
+public:
+    TableOpenError(const std::string& msg):msg_(msg){}
+    const char* what() const noexcept override{
+        return ("TableOpenError:"+msg_).c_str();
+    }
+private:
+    std::string msg_;
+};
+
+class TableDataError: public std::exception
+{
+public:
+    TableDataError(const std::string& msg):msg_(msg){}
+    const char* what() const noexcept override{
+        return ("TableDataError:"+msg_).c_str();
+    }
+private:
+    std::string msg_;
+};
+
+class TableIndexError: public std::exception
+{
+public:
+    TableIndexError(const std::string& msg):msg_(msg){}
+    const char* what() const noexcept override{
+        return ("TableIndexError:"+msg_).c_str();
+    }
+private:
+    std::string msg_;
+};
+
+class TableIndexExist: public std::exception
+{
+public:
+    TableIndexExist(const std::string& msg):msg_(msg){}
+    const char* what() const noexcept override{
+        return ("TableIndexExist:"+msg_).c_str();
+    }   
+private:
+    std::string msg_;
+}; 
+
+class TableCreateError: public std::exception
+{
+public:
+    TableCreateError(const std::string& msg):msg_(msg){}
+    const char* what() const noexcept override{
+        return ("TableCreateError:"+msg_).c_str();
     }
 private:
     std::string msg_;
