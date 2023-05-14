d6bf90672758b9d8a0aadf0ac1fe59e11d70aca0
Rowlet <2474095154@qq.com>
2023-05-14 14:41:42 +0800

    新增DBmanager用于管理数据库的所有表,同时将DB_api中的功能向DBmanager迁移

---
+++ database/DBmanager.cpp
@@ -0,0 +1,116 @@
+#include "DBmanager.hpp"
+#include "MyException.hpp"
+#include <filesystem>
+#include <algorithm>
+DBmanager::DBmanager():path_(""){
+
+}
+DBmanager::DBmanager(const string& path):path_(path)
+{
+    //设置数据库所在根目录
+}
+void DBmanager::UseDatabase(const string& db_name)
+{
+    //打开数据库
+    //先从磁盘中检查是否存在数据库,不存在则丢出异常
+    filesystem::path db_path(path_ + "/" + db_name);
+    if(!filesystem::exists(db_path)){
+        throw DBManagerError("Database " + db_name + " does not exist!");
+    }
+    db_name_ = db_name;
+    //打开数据库中的表,表的后缀名为.table
+    for (auto &p : filesystem::directory_iterator(db_path))
+    {
+        if(p.path().extension() == ".table"){
+            string tb_name = p.path().filename().string();
+            tb_name_list_.push_back(tb_name);
+            tb_list_[tb_name] = make_unique<Table>(Table(db_path.string(),tb_name));
+        }
+    }
+}
+void DBmanager::CreateDatabase(const string& db_name)
+{
+    //新建数据库
+    filesystem::path db_path(path_ + "/" + db_name);
+    if(!filesystem::exists(db_path)){
+        filesystem::create_directory(db_path);
+        db_name_ = db_name;
+    }
+    else{
+        throw DBManagerError("Database " + db_name + " already exists!");
+    }
+}
+void DBmanager::DropDatabase(const string& db_name)
+{
+    //删除数据库
+    filesystem::path db_path(path_ + "/" + db_name);
+    if(filesystem::exists(db_path)){
+        filesystem::remove_all(db_path);
+        db_name_ = "";
+    }
+    else{
+        throw DBManagerError("Database " + db_name + " does not exist!");
+    }
+}
+void DBmanager::CloseDatabase()
+{
+    //关闭数据库
+    db_name_ = "";
+}
+void DBmanager::ShowTables()
+{
+    //展示数据库中的表
+    //表列表以JSON格式输出到标准输入输出流中
+    //JSON格式如下
+    // { "tables": [ "tb1", "tb2", "tb3" ] }
+    cout << "{ \"tables\": [ ";
+    for (auto &p : tb_name_list_)
+    {
+        cout << "\"" << p << "\", ";
+    }
+    cout << "] }" << endl;
+}
+void DBmanager::CreateTable(const string& tb_name,vector<TableColAttribute> &col_info)
+{
+    //新建表
+    //先检查表是否存在,若存在则抛出异常
+    if(tb_list_.find(tb_name) != tb_list_.end()){
+        throw DBManagerError("Table " + tb_name + " already exists!");
+    }
+    tb_name_list_.push_back(tb_name);
+    tb_list_[tb_name] = make_unique<Table>(Table(path_ + "/" + db_name_,tb_name,col_info));
+}
+void DBmanager::DropTable(const string& tb_name)
+{
+    //删除表
+    //先检查表是否存在,若不存在则抛出异常
+    if(tb_list_.find(tb_name) == tb_list_.end()){
+        throw DBManagerError("Table " + tb_name + " does not exist!");
+    }
+    tb_name_list_.erase(find(tb_name_list_.begin(),tb_name_list_.end(),tb_name));
+    //调用Table的删除函数,以删除表文件和对应索引文件
+    tb_list_[tb_name]->DropTable();
+    tb_list_.erase(tb_name);
+}
+void DBmanager::CreateIndex(const string& tb_name,const string& col_name,const string& index_name)
+{
+    //新建索引
+    //先检查表是否存在,若不存在则抛出异常
+    if(tb_list_.find(tb_name) == tb_list_.end()){
+        throw DBManagerError("Table " + tb_name + " does not exist!");
+    }
+    //调用Table的新建索引函数
+    tb_list_[tb_name]->CreateIndex(col_name,index_name);
+}
+void DBmanager::DropIndex(const string& index_name)
+{
+    //删除索引
+    //先去目录下检查索引是否存在,若不存在则抛出异常,后缀名为.idx
+    filesystem::path index_path(path_ + "/" + db_name_ + "/" + index_name + ".idx");
+    if(!filesystem::exists(index_path)){
+        throw DBManagerError("Index " + index_name + " does not exist!");
+    }
+    //删除索引文件
+    filesystem::remove(index_path);
+    //索引信息的删除由Table类完成,这里不做处理
+}
--- database/db_api.hpp
+++ database/db_api.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "SQLstatement.hpp"
+#include "DBmanager.hpp"
 #include <memory>
 // 提供了数据库增删改查操作所需的API
 class DB_API
@@ -8,8 +9,10 @@ private:
     // 我们使用如下两个变量确定数据库位置
     string path_; // 数据库所在文件夹
     string db_name_; // 数据库名字
+    unique_ptr<DBmanager> db_manager_; // 数据库管理器
 public:
     DB_API(const string &path);
+    DB_API(const DB_API &api);
     ~DB_API();
     // 退出数据库(与数据库断开链接)
     void Quit();
@@ -20,7 +23,7 @@ public:
     // 创建表(应该传入一个能描述表结构的结构体)
     void CreateTable(SQLCreateTable &statement);
 
-    // 为表创建一个索引,应该传入表和相fA关的信息
+    // 为表创建一个索引,应该传入表和相关的信息
     void CreateIndex(SQLCreateIndex &statement);
 
     // 显示数据库条目
